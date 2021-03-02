4b457dc7151cf9f18a06a2e852aeec8044e17d60
darkpanda08 <Vineet Ranjan>
2021-03-02 16:35:32 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+console.log(`
+Vineet Ranjan - Student
+
+I'm a student pursuing Bachelor of Engineering in Electrical and Electronics Engineering I'm a Electrical and Electronics Engineering Student who thrives to learn new technologies and work on them. My interests range from Web Development, programming to electrical and electronics designs.
+
+🐥 Twitter: https://twitter.com/vineetranjan08
+📖 GitHub: https://github.com/darkpanda08
+🏢 LinkedIn: https://linkedin.com/in/darkpanda08
+🌐 Website: https://thedarkpanda.tech
+
+`)
