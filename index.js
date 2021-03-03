0f9556e3f94a9efd83c37e59f6f97d5cd65a3170
darkpanda08 <vineetranjan65@gmail.com>
2021-03-03 17:22:00 +0530

    Colors Added

--- index.js
+++ index.js
@@ -2,26 +2,28 @@
 
 const pkgJSON = require('./package.json')
 const welcome = require('cli-welcome')
+const chalk = require('chalk')
+const dim = chalk.dim
+const italic = chalk.italic
+const twitterClr = chalk.hex(`#1da1f2`).bold.inverse
+const githubClr = chalk.hex(`#6cc644`).bold.inverse
+const purple = chalk.hex(`#6937ff`).bold.inverse
 
 welcome({
-    title: pkgJSON.name,
+    title: `Vineet Ranjan`,
     tagLine: `Heyy, nice to meet you !`,
     description: pkgJSON.description,
     version: pkgJSON.version,
-    bgColor: `#FADC00`,
+    bgColor: `#6937ff`,
     color: `#000000`,
     bold: true,
     clear: true
 })
 
-console.log(`
-Vineet Ranjan - Student
+console.log(`${italic(`I'm a student pursuing Bachelor of Engineering in Electrical and Electronics Engineering I'm a Electrical and Electronics Engineering Student who thrives to learn new technologies and work on them. My interests range from Web Development, programming to electrical and electronics designs.`)}
 
-I'm a student pursuing Bachelor of Engineering in Electrical and Electronics Engineering I'm a Electrical and Electronics Engineering Student who thrives to learn new technologies and work on them. My interests range from Web Development, programming to electrical and electronics designs.
-
-🐥 Twitter: https://twitter.com/vineetranjan08
-📖 GitHub: https://github.com/darkpanda08
-🏢 LinkedIn: https://linkedin.com/in/darkpanda08
-🌐 Website: https://thedarkpanda.tech
+${twitterClr(` Twitter `)}: ${dim(`https://twitter.com/vineetranjan08`)}
+${githubClr(` GitHub `)}: ${dim(`https://github.com/darkpanda08`)}
+${purple(` Website `)}: ${dim(`https://thedarkpanda.tech`)}
 
 `)
